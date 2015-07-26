30346b4a4ce0e10fc54934e53acb409c6406b002
jpiatek <jpiatek@chicagobooth.edu>
2015-07-25 19:01:11 -0500

    getting started

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,23 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+
+    @photoid = params["id"]
+    p = Photo.all
+    @photo = @list_of_photos
+
+
+
+    # Photo.find({ :id => "2"})
+
+    # Photo.where({:id => "1"}).show(photo.source)
+
+      "http://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Lake_Bondhus_Norway_2862.jpg/1280px-Lake_Bondhus_Norway_2862.jpg"
+
+    render "application.html.erb"
+
+  end
+
 
 end
